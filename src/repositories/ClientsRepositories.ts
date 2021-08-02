37d53bd24c6a657c4a6eb93d8c06457d950e9407
Ariele Mendonça <ariele_apm@outlook.com>
2021-08-02 20:50:16 -0300

    Add reposiroty client

---
+++ src/repositories/ClientsRepositories.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { Client } from "../entities/Client";
+
+@EntityRepository(Client)
+class ClientRepositories extends Repository<Client> {}
+
+export { ClientRepositories };
